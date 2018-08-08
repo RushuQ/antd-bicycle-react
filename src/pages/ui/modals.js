5774ab370e5c4f75fc2cc613f2518ee586e7e0e0
CRS <RushuQ@users.noreply.github.com>
2018-08-08 10:28:24 +0800

    完善弹框组件

--- src/pages/ui/modals.js
+++ src/pages/ui/modals.js
@@ -3,8 +3,28 @@ import React from 'react';
 import { Card, Button, Modal } from 'antd';
 
 export default class Modals extends React.Component {
+    state = {
+        showModal1: false,
+        showModal2: false,
+        showModal3: false,
+        showModal4: false
+    }
     handleOpen = (type) => {
-
+        this.setState({
+            [type]: true
+        })
+    }
+    handleConfirm = (type) => {
+        Modal[type]({
+            title: '确认?',
+            content: '你确定你学会了React了吗？',
+            onOk() {
+                console.log('OK')
+            },
+            onCancel() {
+                console.log('Cancel');
+            }
+        })
     }
     render() {
         return (
@@ -22,7 +42,54 @@ export default class Modals extends React.Component {
                     <Button type="primary" onClick={() => this.handleConfirm('success')}>Success</Button>
                     <Button type="primary" onClick={() => this.handleConfirm('warning')}>Warning</Button>
                 </Card>
-                
+                <Modal
+                    title="React"
+                    visible={this.state.showModal1}
+                    onCancel={() => {
+                        this.setState({
+                            showModal1: false
+                        })
+                    }}
+                >
+                    <p>欢迎学习慕课新推出的React高级课程</p>
+                </Modal>
+                <Modal
+                    title="React"
+                    visible={this.state.showModal2}
+                    okText="好的"
+                    cancelText="算了"
+                    onCancel={() => {
+                        this.setState({
+                            showModal2: false
+                        })
+                    }}
+                >
+                    <p>欢迎学习慕课新推出的React高级课程</p>
+                </Modal>
+                <Modal
+                    title="React"
+                    style={{ top: 20 }}
+                    visible={this.state.showModal3}
+                    onCancel={() => {
+                        this.setState({
+                            showModal3: false
+                        })
+                    }}
+                >
+                    <p>欢迎学习慕课新推出的React高级课程</p>
+                </Modal>
+                <Modal
+                    title="React"
+                    wrapClassName="vertical-center-modal"
+                    visible={this.state.showModal4}
+                    onCancel={() => {
+                        this.setState({
+                            showModal4: false
+                        })
+                    }}
+                >
+                    <p>欢迎学习慕课新推出的React高级课程</p>
+                </Modal>
             </div>
         )
     }
